388228742e7ab67a52f815742f6adb2ffa14dbca
DAX <dax8tc@gmail.com>
2021-11-01 19:50:09 +0000

    Awesome Solidity Ropsten Hardhat dapp

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,26 @@
 require("@nomiclabs/hardhat-waffle");
+//require("hardhat-typechain");
+// require("@nomiclabs/hardhat-web3");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
+/*
+task("balance", "Prints an account's balance")
+.addParam("account", "The account's address")
+.setAction(async taskArgs => {
+  const account = web3.utils.toChecksumAddress(taskArgs.account);
+  const balance = await web3.eth.getBalance(account);
+
+  console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+});
+*/
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -17,5 +28,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+     ropsten: {
+       //url: "https://ropsten.infura.io/v3/projectid",
+       url: "https://ropsten.infura.io/v3/7e38012c4dc5453fb5db9a07724f3415",
+       accounts: ["0x${process.env.ACCOUNT_KEY}"]
+     }/*,
+     rinkeby: {
+       url: "https://rinkeby.infura.io/v3/projectid",
+       accounts: [process.env.ACCOUNT_KEY]
+     }*/
+  },
   solidity: "0.8.4",
 };
